241cea56472e07b418685245e1d0067138dba881
Suhail7985 <suhailmansoori7985@gmail.com>
2025-07-14 16:55:43 +0530

    frontendupadate

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-export default function Users() {
+
+function LastUpdate({dt}) {
+  const updatedAt = new Date(dt);
+  const diffInMs = Date.now() - updatedAt.getTime();
+  const diffInDays = Math.ceil(diffInMs / 60000 / (60 * 24));
+  return diffInDays + "d";
+}
+
+export default function Users1() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState();
+  const [page, setPage] = useState(1);
+  const [searchVal, setSearchVal] = useState("");
+  const [limit, setLimit] = useState(3);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const formRef = useRef();
 
-  const[limit,setLimit]= useState(3);
-  const[page,setPage]=useState(1);
-  const[searchVal,setsearchVal]=useState("");
-  const[totalPages,setTotalPages]=useState(1);
-
-
+  const [form, setForm] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    role: "",
+  });
   const API_URL = import.meta.env.VITE_API_URL;
   const fetchUsers = async () => {
     try {
-      setError("Loading...");
-      // const url = `${API_URL}/api/users/`;
+      setLoading("Loading...");
       const url = `${API_URL}/api/users/?page=${page}&limit=${limit}&search=${searchVal}`;
       const result = await axios.get(url);
       setUsers(result.data.users);
       setTotalPages(result.data.total);
-      setError();
+      setLoading();
     } catch (err) {
       console.log(err);
       setError("Something went wrong");
@@ -43,25 +59,167 @@ export default function Users() {
     }
   };
 
-  //update user
-  
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
+  const handleEdit = (user) => {
+    setError();
+    setForm(user);
+    setEditId(user._id);
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const frm = formRef.current;
+    if (!frm.checkValidity()) {
+      frm.reportValidity();
+      return;
+    }
+    const url = `${API_URL}/api/users/${editId}`;
+    const result = await axios.patch(url, form);
+    setError("User details modified successfully.");
+    resetForm();
+  };
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    const frm = formRef.current;
+    if (!frm.checkValidity()) {
+      frm.reportValidity();
+      return;
+    }
+    const url = `${API_URL}/api/users`;
+    const result = await axios.post(url, form);
+    setError("New user added successfully.");
+    resetForm();
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setForm({
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      role: "",
+    });
+    setEditId(null);
+    fetchUsers();
+  };
+
+  // const handleSubmit = async (e) => {
+  //   try {
+  //     e.preventDefault();
+  //     if (editId === "edit") {
+  //       const url = `${API_URL}/api/users/${editId}`;
+  //       const result = await axios.patch(url, form);
+  //       setError("User details modified successfully.");
+  //     } else if (editId === "add") {
+  //       const url = `${API_URL}/api/users`;
+  //       const result = await axios.post(url, form);
+  //       setError("New user added successfully.");
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //     setError("Something went wrong");
+  //   }
+  // };
+  const handleSearch = () => {
+    setPage(1);
+    setTotalPages(1);
+    fetchUsers();
+  };
   return (
     <div>
       <div>
         <h2>User Management</h2>
+        {error}
         <div>
-          <input type="text" placeholder="First Name" onChange={(e)=> setsearchVal(e.target.value)}></input>
-          <button onClick={() =>fetchUsers()}>Search</button>
-         
+          <form ref={formRef}>
+            <input
+              type="text"
+              name="firstname"
+              value={form.firstname}
+              placeholder="First name"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              name="lastname"
+              value={form.lastname}
+              placeholder="Last name"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              name="email"
+              value={form.email}
+              placeholder="Email Address"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              placeholder="New Password"
+              onChange={handleChange}
+              required
+            />
+            <select
+              name="role"
+              value={form.role}
+              required
+              onChange={handleChange}
+            >
+              <option value="">--Select Role --</option>
+              <option value="user">User</option>
+              <option value="admin">Admin</option>
+            </select>
+            {/* <input
+              type="text"
+              name="role"
+              value={form.role}
+              placeholder="Role"
+              onChange={handleChange}
+            /> */}
+            {editId ? (
+              <>
+                <button onClick={handleUpdate}>Update</button>
+                <button onClick={handleCancel}>Cancel</button>
+              </>
+            ) : (
+              <button onClick={handleAdd}>Add</button>
+            )}
+          </form>
+        </div>
+        <br />
+        <div>
+          <input
+            type="text"
+            placeholder="First name"
+            onChange={(e) => setSearchVal(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        <br />
+        {loading}
+        <div>
           <table border="1">
             <thead>
               <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
+                <th>First name</th>
+                <th>Last name</th>
                 <th>Email address</th>
                 <th>Role</th>
-         
+                <th>Updated</th>
                 <th>Action</th>
               </tr>
             </thead>
@@ -74,6 +232,10 @@ export default function Users() {
                     <td>{user.email}</td>
                     <td>{user.role}</td>
                     <td>
+                      <LastUpdate dt={user.updatedAt} />
+                    </td>
+                    <td>
+                      <button onClick={() => handleEdit(user)}>Edit</button>
                       <button onClick={() => handleDelete(user._id)}>
                         Delete
                       </button>
@@ -83,15 +245,159 @@ export default function Users() {
               ))}
             <tfoot></tfoot>
           </table>
-        </div>
-        <div>
-          <button disabled={page===1} onClick={()=> setPage(page-1)} >Previous</button>
-          Page {page} of {totalPages}
-          <button disabled ={page===totalPages} onClick={()=> setPage(page+1)}>Next</button>
-
+          <br />
+          <div>
+            <button
+              disabled={page === 1}
+              onClick={() => setPage(Math.max(page - 1, 1))}
+            >
+              Previous
+            </button>
+            Page {page} of {totalPages}
+            <button
+              disabled={page === totalPages}
+              onClick={() => setPage(Math.min(page + 1, totalPages))}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
+
+
+
+
+// import React from "react";
+// import { useEffect, useState, useRef } from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+
+// export default function Users() {
+//   const [users, setUsers] = useState([]);
+//   const [error, setError] = useState();
+//   const [limit, setLimit] = useState(3);
+//   const [form, setForm] = useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     password: "",
+//     role: "",
+//   });
+//   const [page, setPage] = useState(1);
+//   const [searchVal, setSearchVal] = useState("");
+//   const [totalPages, setTotalPages] = useState(1);
+
+//   const API_URL = import.meta.env.VITE_API_URL;
+//   const fetchUsers = async () => {
+//     try {
+//       const url = `${API_URL}/api/users/?page=${page}&limit=${limit}&search=${searchVal}`;
+//       const result = await axios.get(url);
+//       setUsers(result.data.users);
+//       setTotalPages(result.data.total);
+//     } catch (err) {
+//       console.log(err);
+//       setError("Something went wrong");
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, [page]);
+
+//   const handleDelete = async (id) => {
+//     try {
+//       const url = `${API_URL}/api/users/${id}`;
+//       const result = await axios.delete(url);
+//       setError("User deleted successfully");
+//       fetchUsers();
+//     } catch (err) {
+//       console.log(err);
+//       setError("Something went wrong");
+//     }
+//   };
+
+//   const handleAdd = async () => {
+//     try {
+//       const url = `${API_URL}/api/users`;
+//       const result = await axios.post(url,form);
+//       fetchUsers()
+//     } catch (err) {
+//       console.log(err);
+//       setError("Something went wrong");
+//     }
+//   };
+
+//   const handleChange = (e) => {
+//     setForm({...form,[e.target.name]:e.target.value})
+//   }
+
+//   return (
+//     <div>
+//       <div>
+//         <h2>User Management</h2>
+//         <div>
+//           <input name="firstName" value={form.firstName} onChange={handleChange} type="text"  placeholder="First Name" />
+//           <input name="lastName" value={form.lastName} onChange={handleChange} type="text" placeholder="Last Name" />
+//           <input name="email" value={form.email} onChange={handleChange} type="text" placeholder="Email" />
+//           <input name="password" value={form.password} onChange={handleChange} type="password" placeholder="Password" />
+//           <input name="role" value={form.role} onChange={handleChange} type="text" placeholder="Role" />
+//           <button onClick={handleAdd}>Add</button>
+//         </div>
+//         <div>
+//           <input
+//             type="text"
+//             placeholder="First Name"
+//             onChange={(e) => setSearchVal(e.target.value)}
+//           />
+//           <button onClick={() => fetchUsers()}>Search</button>
+//         </div>
+//         <div>
+//           <table border="1">
+//             <thead>
+//               <tr>
+//                 <th>First Name</th>
+//                 <th>Last Name</th>
+//                 <th>Email address</th>
+//                 <th>Role</th>
+
+//                 <th>Action</th>
+//               </tr>
+//             </thead>
+//             {users &&
+//               users.map((user) => (
+//                 <tbody key={user._id}>
+//                   <tr>
+//                     <td>{user.firstName}</td>
+//                     <td>{user.lastName}</td>
+//                     <td>{user.email}</td>
+//                     <td>{user.role}</td>
+//                     <td>
+//                       <button onClick={() => handleDelete(user._id)}>
+//                         Delete
+//                       </button>
+//                     </td>
+//                   </tr>
+//                 </tbody>
+//               ))}
+//             <tfoot></tfoot>
+//           </table>
+//         </div>
+//         <div>
+//           <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+//             Previous
+//           </button>
+//           Page {page} of {totalPages}
+//           <button
+//             disabled={page === totalPages}
+//             onClick={() => setPage(page + 1)}
+//           >
+//             Next
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
